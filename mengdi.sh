6fbf979576b1140987f07ba913508c630665f779
marisn <marisn@67cc.cn>
2019-01-10 10:54:10 +0800

    Create mengdi.sh

---
+++ mengdi.sh
@@ -0,0 +1,337 @@
+#!/bin/bash
+#Check Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+config="/root/shadowsocks/userapiconfig.py"
+get_ip() {
+	ip=$(curl -s https://ipinfo.io/ip)
+	[[ -z $ip ]] && ip=$(curl -s https://api.ip.sb/ip)
+	[[ -z $ip ]] && ip=$(curl -s https://api.ipify.org)
+	[[ -z $ip ]] && ip=$(curl -s https://ip.seeip.org)
+	[[ -z $ip ]] && ip=$(curl -s https://ifconfig.co/ip)
+	[[ -z $ip ]] && ip=$(curl -s https://api.myip.com | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+	[[ -z $ip ]] && ip=$(curl -s icanhazip.com)
+	[[ -z $ip ]] && ip=$(curl -s myip.ipip.net | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+	[[ -z $ip ]] && echo -e "\n 这小鸡鸡还是割了吧！\n" && exit
+}
+Libtest(){
+	#自动选择下载节点
+	GIT='raw.githubusercontent.com'
+	LIB='download.libsodium.org'
+	GIT_PING=`ping -c 1 -w 1 $GIT|grep time=|awk '{print $7}'|sed "s/time=//"`
+	LIB_PING=`ping -c 1 -w 1 $LIB|grep time=|awk '{print $7}'|sed "s/time=//"`
+	echo "$GIT_PING $GIT" > ping.pl
+	echo "$LIB_PING $LIB" >> ping.pl
+	libAddr=`sort -V ping.pl|sed -n '1p'|awk '{print $2}'`
+	if [ "$libAddr" == "$GIT" ];then
+		libAddr='https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.13.tar.gz'
+	else
+		libAddr='https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz'
+	fi
+	rm -f ping.pl		
+}
+Get_Dist_Version()
+{
+    if [ -s /usr/bin/python3 ]; then
+        Version=`/usr/bin/python3 -c 'import platform; print(platform.linux_distribution()[1][0])'`
+    elif [ -s /usr/bin/python2 ]; then
+        Version=`/usr/bin/python2 -c 'import platform; print platform.linux_distribution()[1][0]'`
+    fi
+}
+python_test(){
+	#测速决定使用哪个源
+	tsinghua='pypi.tuna.tsinghua.edu.cn'
+	pypi='mirror-ord.pypi.io'
+	doubanio='pypi.doubanio.com'
+	pubyun='pypi.pubyun.com'	
+	tsinghua_PING=`ping -c 1 -w 1 $tsinghua|grep time=|awk '{print $8}'|sed "s/time=//"`
+	pypi_PING=`ping -c 1 -w 1 $pypi|grep time=|awk '{print $8}'|sed "s/time=//"`
+	doubanio_PING=`ping -c 1 -w 1 $doubanio|grep time=|awk '{print $8}'|sed "s/time=//"`
+	pubyun_PING=`ping -c 1 -w 1 $pubyun|grep time=|awk '{print $8}'|sed "s/time=//"`
+	echo "$tsinghua_PING $tsinghua" > ping.pl
+	echo "$pypi_PING $pypi" >> ping.pl
+	echo "$doubanio_PING $doubanio" >> ping.pl
+	echo "$pubyun_PING $pubyun" >> ping.pl
+	pyAddr=`sort -V ping.pl|sed -n '1p'|awk '{print $2}'`
+	if [ "$pyAddr" == "$tsinghua" ]; then
+		pyAddr='https://pypi.tuna.tsinghua.edu.cn/simple'
+	elif [ "$pyAddr" == "$pypi" ]; then
+		pyAddr='https://mirror-ord.pypi.io/simple'
+	elif [ "$pyAddr" == "$doubanio" ]; then
+		pyAddr='http://pypi.doubanio.com/simple --trusted-host pypi.doubanio.com'
+	elif [ "$pyAddr" == "$pubyun_PING" ]; then
+		pyAddr='http://pypi.pubyun.com/simple --trusted-host pypi.pubyun.com'
+	fi
+	rm -f ping.pl
+}
+install_centos_ssr(){
+	cd /root
+	Get_Dist_Version
+	if [ $Version == "7" ]; then
+		wget --no-check-certificate https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
+		rpm -ivh epel-release-latest-7.noarch.rpm	
+	else
+		wget --no-check-certificate https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+		rpm -ivh epel-release-latest-6.noarch.rpm
+	fi
+	rm -rf *.rpm
+	yum -y update --exclude=kernel*	
+	yum -y install git gcc python-setuptools lsof lrzsz python-devel libffi-devel openssl-devel iptables
+	yum -y update nss curl libcurl 
+	yum -y groupinstall "Development Tools" 
+	#第一次yum安装 supervisor pip
+	yum -y install supervisor python-pip
+	supervisord
+	#第二次pip supervisor是否安装成功
+	if [ -z "`pip`" ]; then
+    curl -O https://bootstrap.pypa.io/get-pip.py
+		python get-pip.py 
+		rm -rf *.py
+	fi
+	if [ -z "`ps aux|grep supervisord|grep python`" ]; then
+    pip install supervisor
+    supervisord
+	fi
+	#第三次检测pip supervisor是否安装成功
+	if [ -z "`pip`" ]; then
+		if [ -z "`easy_install`"]; then
+    wget http://peak.telecommunity.com/dist/ez_setup.py
+		python ez_setup.py
+		fi		
+		easy_install pip
+	fi
+	if [ -z "`ps aux|grep supervisord|grep python`" ]; then
+    easy_install supervisor
+    supervisord
+	fi
+	pip install --upgrade pip
+	Libtest
+	wget --no-check-certificate $libAddr
+	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
+	./configure && make -j2 && make install
+	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+	ldconfig
+	git clone -b manyuser https://github.com/NimaQu/shadowsocks.git "/root/shadowsocks"
+	cd /root/shadowsocks
+	chkconfig supervisord on
+	#第一次安装
+	python_test
+	pip install -r requirements.txt -i $pyAddr	
+	#第二次检测是否安装成功
+	if [ -z "`python -c 'import requests;print(requests)'`" ]; then
+		pip install -r requirements.txt #用自带的源试试再装一遍
+	fi
+	#第三次检测是否成功
+	if [ -z "`python -c 'import requests;print(requests)'`" ]; then
+		mkdir python && cd python
+		git clone https://github.com/shazow/urllib3.git && cd urllib3
+		python setup.py install && cd ..
+		git clone https://github.com/nakagami/CyMySQL.git && cd CyMySQL
+		python setup.py install && cd ..
+		git clone https://github.com/requests/requests.git && cd requests
+		python setup.py install && cd ..
+		git clone https://github.com/pyca/pyopenssl.git && cd pyopenssl
+		python setup.py install && cd ..
+		git clone https://github.com/cedadev/ndg_httpsclient.git && cd ndg_httpsclient
+		python setup.py install && cd ..
+		git clone https://github.com/etingof/pyasn1.git && cd pyasn1
+		python setup.py install && cd ..
+		rm -rf python
+	fi	
+	systemctl stop firewalld.service
+	systemctl disable firewalld.service
+	cd /root/shadowsocks
+	cp apiconfig.py userapiconfig.py
+	cp config.json user-config.json
+}
+install_ubuntu_ssr(){
+	apt-get update -y
+	apt-get install supervisor lsof -y
+	apt-get install build-essential wget -y
+	apt-get install iptables git -y
+	Libtest
+	wget --no-check-certificate $libAddr
+	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
+	./configure && make -j2 && make install
+	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+	ldconfig
+	apt-get install python-pip git -y
+	pip install cymysql
+	cd /root
+	git clone -b manyuser https://github.com/NimaQu/shadowsocks.git "/root/shadowsocks"
+	cd shadowsocks
+	pip install -r requirements.txt
+	chmod +x *.sh
+	# 配置程序
+	cp apiconfig.py userapiconfig.py
+	cp config.json user-config.json
+}
+install_node(){
+	clear
+	#Check Root
+	[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+	#check OS version
+	check_sys(){
+		if [[ -f /etc/redhat-release ]]; then
+			release="centos"
+		elif cat /etc/issue | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+		elif cat /proc/version | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /proc/version | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+	  fi
+	}
+	install_ssr_for_each(){
+		check_sys
+		if [[ ${release} = "centos" ]]; then
+			install_centos_ssr
+		else
+			install_ubuntu_ssr
+		fi
+	}
+	clear
+	# 取消文件数量限制
+	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
+	echo -e "如果以下手动配置错误，请在${config}手动编辑修改"
+	read -p "请输入你的对接域名或IP(例如:http://www.baidu.com 默认为本机对接): " WEBAPI_URL
+	read -p "请输入muKey(在你的配置文件中 默认marisn):" WEBAPI_TOKEN
+	read -p "请输入测速周期(回车默认为每6小时测速):" SPEEDTEST
+	read -p "请输入你的节点编号(回车默认为节点ID 3):  " NODE_ID
+	install_ssr_for_each
+	cd /root/shadowsocks
+	echo -e "modify Config.py...\n"
+	get_ip
+	WEBAPI_URL=${WEBAPI_URL:-"http://${ip}"}
+	sed -i '/WEBAPI_URL/c \WEBAPI_URL = '\'${WEBAPI_URL}\''' ${config}
+	#sed -i "s#https://zhaoj.in#${WEBAPI_URL}#" /root/shadowsocks/userapiconfig.py
+	WEBAPI_TOKEN=${WEBAPI_TOKEN:-"marisn"}
+	sed -i '/WEBAPI_TOKEN/c \WEBAPI_TOKEN = '\'${WEBAPI_TOKEN}\''' ${config}
+	#sed -i "s#glzjin#${WEBAPI_TOKEN}#" /root/shadowsocks/userapiconfig.py
+	SPEEDTEST=${SPEEDTEST:-"6"}
+	sed -i '/SPEED/c \SPEEDTEST = '${SPEEDTEST}'' ${config}
+	NODE_ID=${NODE_ID:-"3"}
+	sed -i '/NODE_ID/c \NODE_ID = '${NODE_ID}'' ${config}
+	# 启用supervisord
+	supervisorctl shutdown
+	#某些机器没有echo_supervisord_conf 
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/supervisord.conf
+	supervisord
+	#iptables
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
+	chmod +x /etc/rc.d/rc.local
+	echo "#############################################################"
+	echo "#        安装完成，节点即将重启使配置生效                   #"
+	echo "#############################################################"
+	reboot now
+}
+install_node_db(){
+	clear
+	#Check Root
+	[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+	#check OS version
+	check_sys(){
+		if [[ -f /etc/redhat-release ]]; then
+			release="centos"
+		elif cat /etc/issue | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+		elif cat /proc/version | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /proc/version | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+	  fi
+	}
+	install_ssr_for_each(){
+		check_sys
+		if [[ ${release} = "centos" ]]; then
+			install_centos_ssr
+		else
+			install_ubuntu_ssr
+		fi
+	}
+	 clear
+	# 取消文件数量限制
+	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
+	echo -e "如果以下手动配置错误，请在${config}手动编辑修改"
+	read -p "请输入你的对接数据库IP(例如:127.0.0.1 如果是本机请直接回车): " MYSQL_HOST
+	read -p "请输入你的数据库名称(默认sspanel):" MYSQL_DB
+	read -p "请输入你的数据库端口(默认3306):" MYSQL_PORT
+	read -p "请输入你的数据库用户名(默认root):" MYSQL_USER
+	read -p "请输入你的数据库密码(默认root):" MYSQL_PASS
+	read -p "请输入你的节点编号(回车默认为节点ID 3):  " NODE_ID
+	install_ssr_for_each
+	cd /root/shadowsocks
+	echo -e "modify Config.py...\n"
+	get_ip
+	sed -i '/API_INTERFACE/c \API_INTERFACE = '\'glzjinmod\''' ${config}
+	MYSQL_HOST=${MYSQL_HOST:-"${ip}"}
+	sed -i '/MYSQL_HOST/c \MYSQL_HOST = '\'${MYSQL_HOST}\''' ${config}
+	MYSQL_DB=${MYSQL_DB:-"sspanel"}
+	sed -i '/MYSQL_DB/c \MYSQL_DB = '\'${MYSQL_DB}\''' ${config}
+	MYSQL_USER=${MYSQL_USER:-"root"}
+	sed -i '/MYSQL_USER/c \MYSQL_USER = '\'${MYSQL_USER}\''' ${config}
+	MYSQL_PASS=${MYSQL_PASS:-"root"}
+	sed -i '/MYSQL_PASS/c \MYSQL_PASS = '\'${MYSQL_PASS}\''' ${config}
+	MYSQL_PORT=${MYSQL_PORT:-"3306"}
+	sed -i '/MYSQL_PORT/c \MYSQL_PORT = '${MYSQL_PORT}'' ${config}
+	NODE_ID=${NODE_ID:-"3"}
+	sed -i '/NODE_ID/c \NODE_ID = '${NODE_ID}'' ${config}
+	# 启用supervisord
+	supervisorctl shutdown
+	#某些机器没有echo_supervisord_conf 
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/supervisord.conf
+	supervisord
+	#iptables
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
+	chmod +x /etc/rc.d/rc.local
+	echo "#############################################################"
+	echo "#         安装完成，节点即将重启使配置生效                  #"
+	echo "#############################################################"
+	reboot now
+}
+echo
+echo "#############################################################"
+echo "#           Mengdi 节点专用脚本   by：十一                  #"
+echo "#############################################################"
+echo
+echo -e "\033[1;5;31m请选择对接模式：\033[0m"
+echo -e "1.API对接模式"
+echo -e "2.数据库对接模式"
+read -t 30 -p "选择：" NODE_MS
+case $NODE_MS in
+		1)
+			install_node
+			;;
+		2)
+			install_node_db
+			;;
+		*)
+		    echo -e "请选择正确对接模式"
+			exit 1
+			;;
+esac
